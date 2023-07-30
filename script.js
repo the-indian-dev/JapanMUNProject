8eab1bc95f5687329f3f633c287835a95c1862b5
Ritabrata Das <ritabrata.das2669@gmail.com>
2023-07-30 20:42:01 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var counter = 0
+var en_haiku = document.getElementById("haiku-en")
+var jp_haiku = document.getElementById("haiku-jp")
+var haikus = $('#haikus')
+setInterval(change, 5000);
+function change() {
+    haikus.fadeOut(function(){
+        jp_haiku.innerText = (haiku_arr[counter]["haiku"])
+        en_haiku.innerText = (haiku_arr[counter]["translation"])
+        counter++
+        if(counter >= haiku_arr.length) { counter = 0; }
+        haikus.fadeIn()
+    });
+}
